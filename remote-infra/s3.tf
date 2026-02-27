8acfb808c4ff32148b7d5dd66e47674ae4dd38f3
mangesh <mangeshtakras777@gmail.com>
2026-02-27 12:55:00 +0000

    v15

--- remote-infra/s3.tf
+++ remote-infra/s3.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "remote_s3" {
-  bucket = "my-tf-test-bucket_man"
+  bucket = "my-tf-test-bucket-man"
 
   tags = {
-    Name        = "my-tf-test-bucket_man"
+    Name        = "my-tf-test-bucket-man"
     
   }
 }
