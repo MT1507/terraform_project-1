9b494c3569965eea67c60263c1fd99f3b151d554
mangesh <mangeshtakras777@gmail.com>
2026-02-27 06:49:16 +0000

    v6

--- update_code.sh
+++ update_code.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+
 set -e
 
-echo "🔄 Adding changes..."
-git add .
+echo "Choose option:"
+echo "1 - Pull latest code"
+echo "2 - Push local changes"
+read -p "Enter choice: " choice
 
-read -p "Enter commit message: " msg
-git commit -m "$msg"
-
-echo "🚀 Pushing changes..."
-git push origin main
-
-echo "✅ Done! Dana done "
-
+if [ "$choice" == "1" ]; then
+    echo "⬇️ Pulling latest code..."
+    git pull origin main
+    echo "✅ Pull completed!"
+elif [ "$choice" == "2" ]; then
+    echo "🔄 Adding changes..."
+    git add .
+    read -p "Enter commit message: " msg
+    git commit -m "$msg"
+    echo "🚀 Pushing to GitHub..."
+    git push origin main
+    echo "✅ Push completed!"
+else
+    echo "❌ Invalid option"
+fi
