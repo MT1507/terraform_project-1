d55a98c91fbb5c40ed40a76e74c76ee55f8f36b0
Mangesh <mangeshtakras777@gmail.com>
2026-03-02 20:43:31 +0530

    v:2026-03-02_20-43-30 by mange

--- terraform-ec2/terraform.tf
+++ terraform-ec2/terraform.tf
@@ -5,4 +5,11 @@ terraform {
       version = "6.33.0"
     }
   }
+
+  backend "s3" {
+    bucket = "my-tf-test-bucket-man"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "my-tf-test-table"
+  }
 }
